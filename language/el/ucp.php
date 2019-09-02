5c815ac916df400e36aa77a41a1fabff441824dd
Panos <pkonstae@gmail.com>
2019-09-02 14:14:12 +0300

    Αλλαγές 3.2.8-RC1

--- language/el/ucp.php
+++ language/el/ucp.php
@@ -117,7 +117,7 @@ $lang = array_merge($lang, array(
 	'BIRTHDAY'					=> 'Γενέθλια',
 	'BIRTHDAY_EXPLAIN'			=> 'Εισάγοντας το έτος γεννήσεώς σας θα εμφανίζεται και η ηλικία σας την ημέρα γενεθλίων σας.',
 	'BOARD_DATE_FORMAT'			=> 'Μορφή ημερομηνίας μου',
-	'BOARD_DATE_FORMAT_EXPLAIN'	=> 'Η σύνταξη είναι πανομοιότυπη με τη συνάρτηση <a href="http://www.php.net/date">date()</a> της PHP.',
+	'BOARD_DATE_FORMAT_EXPLAIN'	=> 'Η σύνταξη είναι πανομοιότυπη με τη συνάρτηση <a href="https://secure.php.net/manual/function.date.php">date()</a> της PHP.',
 	'BOARD_LANGUAGE'			=> 'Γλώσσα επικοινωνίας μου',
 	'BOARD_STYLE'				=> 'Το στυλ μου',
 	'BOARD_TIMEZONE'			=> 'Η ωρολογιακή ζώνη μου',
